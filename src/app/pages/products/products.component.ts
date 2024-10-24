a3b1834f5674ea8b5396f2b4e7796e6b852f7a58
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2024-10-24 14:40:00 +0530

    implement ngx-spinner

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
+import { NgxLoadingModule } from 'ngx-loading';
+import { NgxSpinnerModule, NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [CardModule,ButtonModule],
+  imports: [CardModule, ButtonModule, NgxSpinnerModule],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.css'
+  styleUrl: './products.component.css',
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  spinner = inject(NgxSpinnerService);
 
+  ngOnInit(): void {
+    /** spinner starts on init */
+    this.spinner.show();
+
+    setTimeout(() => {
+      /** spinner ends after 5 seconds */
+      this.spinner.hide();
+    }, 5000);
+  }
 }
