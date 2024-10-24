e8e38bc87595bb1cbda2bfced37c5e22d32604ae
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2024-10-24 08:38:11 +0530

    implement equal height and width of product list grid

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [CardModule,ButtonModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
